fa52c5886601f7915d7a70da0627554434570a8e
fineopolis <ebouabbdi@gmail.com>
2025-02-26 10:48:19 +0000

    build first route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header>this is header</header>
         {children}
       </body>
     </html>
